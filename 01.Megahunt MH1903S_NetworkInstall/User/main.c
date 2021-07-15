8eba23406822fee135c60c15c2461e5d49d734dc
Leo Wang <tank19950727@gmail.com>
2021-07-15 12:30:44 +0800

    Add routines
    Add routines

---
+++ 01.Megahunt MH1903S_NetworkInstall/User/main.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdint.h>
+#include "mhscpu.h"
+#include "sysc.h"
+#include "delay.h"
+#include "usart.h"
+#include "w5500.h"
+
+///-------------------------------------------------------------------------------------------------
+/// <summary>Netwwork initialize.</summary>
+///
+/// <remarks>Tony Wang, 2021/6/24.</remarks>
+///-------------------------------------------------------------------------------------------------
+
+void Netwwork_Init(void)
+{
+	uint8_t MAC[6] = { 0x00,0x08,0xdc,0x11,0x12,0x13 };
+	uint8_t LocalIp[4] = { 192,168,1,150 };
+	uint8_t Subnet[4] = { 255,255,255,0 };
+	uint8_t Gateway[4] = { 192,168,1,1 };
+
+	uint8_t TxSize[MAX_SOCK_NUM] = { 2,2,2,2,2,2,2,2 };
+	uint8_t RxSize[MAX_SOCK_NUM] = { 2,2,2,2,2,2,2,2 };
+
+	uint8_t temp[4] = { 0 };
+
+	setSHAR(MAC);
+	setSUBR(Subnet);
+	setGAR(Gateway);
+	setSIPR(LocalIp);
+
+	sysinit(TxSize, RxSize);
+
+	setRTR(2000);
+	setRCR(3);
+
+	getSIPR(temp);
+	printf("W5500EVB IP : %d.%d.%d.%d\r\n", temp[0], temp[1], temp[2], temp[3]);
+
+	getSUBR(temp);
+	printf("W5500EVB SN : %d.%d.%d.%d\r\n", temp[0], temp[1], temp[2], temp[3]);
+
+	getGAR(temp);
+	printf("W5500EVB GW : %d.%d.%d.%d\r\n", temp[0], temp[1], temp[2], temp[3]);
+}
+
+int main(void)
+{
+	SystemClock_Config_HSE();
+
+	Delay_Init();
+
+	USART_Init();
+
+	printf("USART_Init\r\n");
+
+	W5500_Init();
+
+	printf("W5500_Init\r\n");
+
+	W5500_Reset();
+
+	printf("W5500_Reset\r\n");
+
+	Netwwork_Init();
+
+	printf("Netwwork_Init\r\n");
+
+	while (1)
+	{
+
+	}
+}
+
+#ifdef  USE_FULL_ASSERT
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{
+	/* User can add his own implementation to report the file name and line number,
+	 ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+	 /* Infinite loop */
+	while (1)
+	{
+	}
+}
+#endif
---
+++ 03.Megahunt MH1903S_TCPClient/User/MH1903SEVB_Driver/inc/type.h
@@ -0,0 +1,6 @@
+#ifndef	__TYPE_H
+#define	__TYPE_H
+
+typedef uint8_t 				SOCKET;
+
+#endif
