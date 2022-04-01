1a10a3b8ea53b4759f650b73c5e2bcbcad00039d
crawford <1443728561@qq.com>
2022-04-01 16:36:47 +0800

    PHY

---
+++ 17.Megahunt MH1903S_PHY/User/mhscpu_it.h
@@ -0,0 +1,30 @@
+#ifndef __MHSCPU_IT_H
+#define __MHSCPU_IT_H
+
+#ifdef __cplusplus
+ extern "C" {
+#endif 
+
+/* Includes ------------------------------------------------------------------*/
+#include "mhscpu.h"
+
+/* Exported types ------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+/* Exported macro ------------------------------------------------------------*/
+/* Exported functions ------------------------------------------------------- */
+
+void NMI_Handler(void);
+void HardFault_Handler(void);
+void MemManage_Handler(void);
+void BusFault_Handler(void);
+void UsageFault_Handler(void);
+void SVC_Handler(void);
+void DebugMon_Handler(void);
+void PendSV_Handler(void);
+void SysTick_Handler(void);
+void DMA0_IRQHandler(void);
+#ifdef __cplusplus
+}
+#endif
+
+#endif 
